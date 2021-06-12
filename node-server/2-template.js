0869299776fd240361edcd30bc2586fb3a46dad1
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-12 12:58:53 +0900

    feat: 템플릿 엔진 추가

---
+++ node-server/2-template.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+// const http = require('http2');
+
+const name = 'Ellie';
+const courses = [{ name: 'HTML' }, { name: 'CSS' }, { name: 'Node' }, { name: 'Frontend' }];
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/html');
+  if (url === '/') {
+    ejs.renderFile('./template/index.ejs', { name }).then((data) => res.end(data));
+  } else if (url === '/courses') {
+    ejs.renderFile('./template/courses.ejs', { courses }).then((data) => res.end(data));
+  } else {
+    ejs.renderFile('./template/not-found.ejs', { name }).then((data) => res.end(data));
+  }
+});
+
+server.listen(8080);
