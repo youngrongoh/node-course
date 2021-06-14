8983925d0081248dd8754b6a2e866e480aab3b6c
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-15 00:45:06 +0900

    feat: 미들웨어 추가

--- cors/app.js
+++ cors/app.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://127.0.0.1:5502',
-    optionsSuccessStatus: 200,
-    credentials: true, // Access-Control-Allow-Credentials: true
-  })
-);
+const corsOptions = {
+  origin: 'http://127.0.0.1:5502',
+  optionsSuccessStatus: 200,
+  credentials: true, // Access-Control-Allow-Credentials: true
+};
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(cors(corsOptions));
 /* 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5502');
@@ -17,7 +24,10 @@ app.use((req, res, next) => {
   next();
 });
  */
+
 app.get('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
   res.send('Welcome!');
 });
 
