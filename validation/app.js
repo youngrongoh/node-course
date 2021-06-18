3acdbbd229ba1d9046791e9e394e62199ced6ddf
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-18 19:55:44 +0900

    refactor: 유효성 에러 처리 로직 분리

--- validation/app.js
+++ validation/app.js
@@ -4,6 +4,14 @@ import { body, param, validationResult } from 'express-validator';
 const app = express();
 app.use(express.json());
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
+
 app.post(
   '/users',
   [
@@ -12,12 +20,8 @@ app.post(
     body('email').isEmail().withMessage('이메일 입력해요'),
     body('job.name').notEmpty(),
   ],
+  validate,
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
-
     console.log(req.body);
     res.sendStatus(201);
   }
@@ -26,11 +30,8 @@ app.post(
 app.get(
   '/:email',
   param('email').isEmail().withMessage('이메일 입력해요'),
+  validate,
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     res.send('send!');
   }
 );
